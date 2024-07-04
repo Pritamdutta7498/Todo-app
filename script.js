f118c2fd41cd6e97aec5068f1a773d0fbc528ea5
pritam dutta <webdevpritam7498@gmail.com>
2024-07-04 20:25:44 +0600

    showing the todoList in UI

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const todoName = document.querySelector(".js-todo-name");
 const todoDate = document.querySelector(".js-todo-date");
-const todoElementList = document.querySelector(".js-todo-list");
+let todoElementList = document.querySelector(".js-todo-list");
 
 function addTodo() {
   const todoNameValue = todoName.value;
@@ -13,17 +13,45 @@ function addTodo() {
     completed: false,
   };
 
-  let todoList= JSON.parse(localStorage.getItem('todoList'));
+  let todoList = JSON.parse(localStorage.getItem("todoList"));
   if (!Array.isArray(todoList)) {
     todoList = [];
-  };
+  }
   todoList.push(todoItem);
 
-  console.log(todoList);
+  // console.log(todoList);
 
   localStorage.setItem("todoList", JSON.stringify(todoList));
 
-  todoName.value = '';
-  todoDate.value = '';
-
+  todoName.value = "";
+  todoDate.value = "";
+  renderTodoList();
 }
+
+function renderTodoList() {
+  let todoList = JSON.parse(localStorage.getItem("todoList"));
+  todoElementList.innerHTML = "";
+console.log(todoList);
+
+  todoList.forEach((todo, i) => {
+    // const div = document.createElement("div");
+    // div.className = "todo-list-data";
+    // const span = document.createElement("span");
+    console.log(todo.name);
+
+   let todoElementData = `
+    <div class='todo-list-data'> 
+      <span>${todo.name}</span>
+      <span>${todo.date}</span>
+      <button onclick="deleteTodo(${i})" class="deleteBtn">Delete
+      </button>
+    </div>
+    `;
+    // append each
+    todoElementList.innerHTML += todoElementData;
+    
+  });
+}
+
+
+renderTodoList();
